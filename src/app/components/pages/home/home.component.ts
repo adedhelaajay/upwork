8daa773cb229c16d7dbad4fc94152b2a97be5be2
Nittile Gupta <gnittile@irasoftwares.com>
2024-02-08 13:17:24 +0530

    Code Update with NewUI

---
+++ src/app/components/pages/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { FormControl,FormControlName, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  contactForm:FormGroup;
+  contactForm2:FormGroup;
+  showdiv:any=false;
+  constructor(private http: HttpClient) { 
+
+    this.contactForm = new FormGroup({
+      fname : new FormControl(""),
+      email : new FormControl("",Validators.email),
+      message: new FormControl("")
+    });
+
+
+    this.contactForm2 = new FormGroup({
+      subscribeemail : new FormControl("",Validators.email),
+    });
+  }
+
+  toggle(){
+    $('.left-nav').addClass('active');
+  }
+  showtoggle(){
+    $('.left-nav').removeClass('active');
+  }
+
+  emailsubmit(contactForm2:FormGroup){
+   // alert("Email"+contactForm2.value.subscribeemail)
+    console.log("Email",contactForm2.value.subscribeemail);
+    
+  }
+  submit(contactForm:FormGroup) {
+    console.log("Name", contactForm.value.fname ,"Email",contactForm.value.email);
+    
+   // alert("Name"+ contactForm.value.fname +"Email"+contactForm.value.email)
+  }
+
+}
